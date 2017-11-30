269899df1074ffc0d067dd7e51f0ee8ef911f7de
AddeNordh <nordh.andreas@hotmail.com>
2017-11-30 22:31:57 +0100

    Added click event aswell as shorting the check before the game

--- js/master.js
+++ js/master.js
@@ -4,19 +4,30 @@ const container = document.getElementById('wrapper');
 const initInput = document.getElementsByClassName('init-game')[0];
 const start = document.getElementById('button');
 const errormsg = document.getElementById('error-msg');
+let ready;
+
+const cardClick = (cards) => {
+	for (let card of cards.cards) {
+		card.addEventListener("click", () => {
+			cards.addTarget(card);
+		})
+	}
+}
 
 initInput.addEventListener("keyup", () => {
-		if (initInput.value % 2 != 0 || initInput.value == "") {
+		if (initInput.value % 2 != 0 || initInput.value == "" || initInput.value == 0) {
+			ready = false;
 			errormsg.innerHTML = "Insufficient number of cards";
 		}
 		else {
+			ready = true;
 			errormsg.innerHTML = "Good to go!";
 		}
 });
 
 start.addEventListener("click", () => {
 
-    if (initInput.value != "" && initInput.value % 2 == 0) {
+    if (ready) {
 		wrapper.style.display = "none";
         let totalCards = initInput.value;
 
@@ -26,7 +37,9 @@ start.addEventListener("click", () => {
         let cards = new Cards(init);
 		cards.shuffle();
 		cards.setCards();
-
+		setTimeout(() => {
+			cardClick(cards);
+		},1000);
     }
     else {
         errormsg.innerHTML = "Insufficient number of cards";
