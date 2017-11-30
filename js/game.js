1e754ef54a286742cd533c8c63a526320855ebcb
AddeNordh <nordh.andreas@hotmail.com>
2017-11-30 18:32:51 +0100

    Seperated the game object to new file

---
+++ js/game.js
@@ -0,0 +1,28 @@
+function Game() {
+
+    this.cards = [];
+    this.values = [];
+    this.value = Math.floor((Math.random()) + Math.random());
+
+    this.init = (length) => {
+        for (let i = 0; i < length; i++) {
+
+            if (i % 2 === 0) {
+                this.value = Math.ceil(Math.random() + 1  * i + 1 % 2);
+                for (let value of this.values) {
+                    if (this.value === value) {
+                        this.value = Math.ceil(Math.random() * i);
+                    }
+                }
+            }
+
+            let cardEle = document.createElement("div");
+            cardEle.classList.add("card");
+            cardEle.setAttribute('data-value', this.value);
+
+            this.values.push(this.value);
+            this.cards.push(cardEle);
+        }
+		return this.cards;
+    }
+}
