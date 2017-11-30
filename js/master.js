3acd6bf3408bef6a8f9c313c5a685aeb49dfe4cb
AddeNordh <nordh.andreas@hotmail.com>
2017-11-30 20:21:47 +0100

    Added more css and some script to check value of input

--- js/master.js
+++ js/master.js
@@ -1,14 +1,23 @@
 "use strict";
 
-const container = document.getElementById('container');
+const container = document.getElementById('wrapper');
 const initInput = document.getElementsByClassName('init-game')[0];
 const start = document.getElementById('button');
 const errormsg = document.getElementById('error-msg');
 
+initInput.addEventListener("keyup", () => {
+		if (initInput.value % 2 != 0 || initInput.value == "") {
+			errormsg.innerHTML = "Insufficient number of cards";
+		}
+		else {
+			errormsg.innerHTML = "Good to go!";
+		}
+});
+
 start.addEventListener("click", () => {
 
     if (initInput.value != "" && initInput.value % 2 == 0) {
-
+		wrapper.style.display = "none";
         let totalCards = initInput.value;
 
         let game = new Game();
